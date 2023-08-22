47784dedfec0c8278a59083a6c38015fdb34df10
kajinmajin <68888885+kajinmajin@users.noreply.github.com>
2023-08-22 11:57:17 +0545

    ajax hit

--- VidhyasDietERP/urls.py
+++ VidhyasDietERP/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from django.contrib import admin
-from vidhyas_api.views import ObtainAuthTokenView
+# from vidhyas_api.views import ObtainAuthTokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
