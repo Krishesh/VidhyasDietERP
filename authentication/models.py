0071f4dee66c9049f1d8016da4e2f83877dab584
Krishesh shrestha <shrestha.krishesh@gmail.com>
2023-08-23 15:47:03 +0545

    human resource mananagement setup

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,3 @@ from django.db import models
 
 # Create your models here.
 
-class test(models.Model):
-    test = models.CharField(max_length=10, default="")
-    bibek = models.CharField(max_length=10, default="")
